db3d6cfca667d5532c548c533f9877004d4ec485
Opilane TTHK <opilane@tthk.ee>
2020-09-15 13:55:06 +0300

    first commit

---
+++ Week3LogicalOperatsrsGR/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Week3LogicalOperatsrsGR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What month were you born?");
+            string month = Console.ReadLine().ToLower();
+            // || - pices alt gr + <
+            if(month == "december" || month == "januar" || month == "februar")
+            {
+                Console.WriteLine("You were born in winter.");
+            }else if(month == "December" || month == "january" || month == "february")
+            {
+                Console.WriteLine("You were born in spring.");
+            }else if(month == "march" || month == "aprill" || month == "may")
+            {
+
+            }
+        }
+    }
+}
